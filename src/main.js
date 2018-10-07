63c97817b9558ae6e856e897d118ea01bee68b0c
Andrew <andrewyakonyuk@gmail.com>
2018-10-07 21:16:09 +0300

    progress bar for async data

--- src/main.js
+++ src/main.js
@@ -3,11 +3,39 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import ProgressBar from './components/ProgressBar.vue';
 
 Vue.config.productionTip = false;
+
+// global progress bar
+const progressBar = Vue.prototype.$progressBar = new Vue(ProgressBar).$mount();
+document.body.appendChild(progressBar.$el);
 
 new Vue({
   router,
   store,
   render: h => h(App),
 }).$mount('#app');
+
+// Add router hook for handling asyncData.
+// Doing it after initial route is resolved so that we don't double-fetch
+// the data that we already have. Using router.beforeResolve() so that all
+// async components are resolved.
+router.beforeResolve((to, from, next) => {
+  const matched = router.getMatchedComponents(to);
+  const prevMatched = router.getMatchedComponents(from);
+  let diffed = false;
+  const activated = matched.filter((c, i) => diffed || (diffed = (prevMatched[i] !== c)));
+  const asyncDataHooks = activated.map(c => c.asyncData).filter(_ => _);
+  if (!asyncDataHooks.length) {
+    return next();
+  }
+
+  progressBar.start();
+  return Promise.all(asyncDataHooks.map(hook => hook({ store, route: to, progressBar })))
+    .then(() => {
+      progressBar.finish();
+      next();
+    })
+    .catch(next);
+});
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,20 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'about',
+  asyncData({ store, route, progressBar }) {
+    console.log({ store, route, progressBar });
+    const promise = new Promise(((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    }));
+
+    return promise;
+  },
+};
+</script>
+
