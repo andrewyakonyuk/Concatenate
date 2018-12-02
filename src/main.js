82408d62f865fcb06fa7e3c5de151e13981aa330
andrewyakonyuk <andrewyakonyuk@gmail.com>
2018-12-02 20:11:52 +0200

    includes font-awesome icons

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'; import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 import ProgressBar from './components/ProgressBar.vue';
+
+library.add(fas);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
