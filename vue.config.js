602611c09d9b333f05a4d473b9776bf30b890589
Andrew <andrewyakonyuk@gmail.com>
2018-12-22 16:54:55 +0200

    add monaco webpack plugin

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/Concatenate/'
     : '/',
+  configureWebpack: {
+    plugins: [
+      new MonacoWebpackPlugin(),
+    ],
+  },
 };
-
